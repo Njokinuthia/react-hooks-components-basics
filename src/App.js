f9e5db396b9ac0338bdc091745f20c2aae0408d0
Njoki <kinuthiash@gmail.com>
2022-05-01 16:04:03 +0300

    complete assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Njoki from "./Njoki"
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Njoki/>
     </div>
   );
 }
---
+++ src/Njoki.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+// // function component
+// function Njoki(){
+//   return(<h3>My function component"</h3>)
+// }
+
+// // arrow function component
+// const Njoki = ()=><h3>My arrow function component</h3>
+
+// class component
+class Njoki extends React.Component{
+  render(){
+    return <p>My class component</p>
+  }
+}
+
+export default Njoki;
